b1590cd56bfaf3a5060a9535e7a56b18548afa7b
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-10-26 19:21:37 +0300

    fix: add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
