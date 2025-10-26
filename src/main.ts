6135c4e75cd1ebd76e812c39509f01906de7a718
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-10-26 19:08:34 +0300

    feat: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Smart Communication Hub')
+    .setDescription(
+      'A smart communication hub that allows users chat with each other get a summary for their chats.'
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
