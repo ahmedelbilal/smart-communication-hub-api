37c3bb628697fba73f92b61bbcc663d2de5e3846
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-10-27 19:23:13 +0300

    feat: datasource

---
+++ data-source.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const certPath = path.resolve(__dirname, 'certs/ca-certificate.crt');
+const ca = fs.readFileSync(certPath).toString();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [join(__dirname, 'src/**/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, 'src/migrations/*{.ts,.js}')],
+  synchronize: false,
+  logging: true,
+  ssl: { ca },
+});
