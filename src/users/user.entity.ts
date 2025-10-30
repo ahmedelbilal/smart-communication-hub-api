9eaceb4e0e7caa9a0396e790bfb24a890470bea6
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-10-30 20:06:13 +0300

    fix: add messages + conversations relations

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,9 +1,12 @@
+import { Conversation } from 'src/conversations/conversation.entity';
+import { Message } from 'src/messages/message.entity';
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from 'typeorm';
 
 @Entity('users')
@@ -19,6 +22,15 @@ export class User {
 
   @Column()
   password: string; // hashed
+
+  @OneToMany(() => Conversation, (conversation) => conversation.user1)
+  conversationsInitiated: Conversation[];
+
+  @OneToMany(() => Conversation, (conversation) => conversation.user2)
+  conversationsReceived: Conversation[];
+
+  @OneToMany(() => Message, (message) => message.sender)
+  messages: Message[];
 
   @CreateDateColumn()
   createdAt: Date;
