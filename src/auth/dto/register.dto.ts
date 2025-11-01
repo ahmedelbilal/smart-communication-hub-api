45967d90cbf696407b2121a62ab43398506d5ec8
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-11-01 09:08:11 +0200

    fix: transform to lowercase

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,15 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, IsStrongPassword, MaxLength, MinLength } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class RegisterDto {
   @ApiProperty({ example: 'user' })
   @IsString()
   @MinLength(1)
   @MaxLength(100)
+  @Transform(({ value }) => value.toLowerCase().trim())
   name: string;
 
   @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
+  @Transform(({ value }) => value.toLowerCase().trim())
   email: string;
 
   @ApiProperty({ example: 'strongPassword123@', minLength: 6 })
