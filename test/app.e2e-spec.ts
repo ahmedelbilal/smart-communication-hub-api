69ded75f66c75d3c242859e91984af385e8dcc5e
ahmedelbilal <ahmed.sudani570@gmail.com>
2025-11-01 21:53:19 +0200

    fix: add insight repo

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,10 +4,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as request from 'supertest';
 import { AuthModule } from '../src/auth/auth.module';
+import { Conversation } from '../src/conversations/conversation.entity';
+import { Insight } from '../src/insights/insight.entity';
+import { Message } from '../src/messages/message.entity';
 import { User } from '../src/users/user.entity';
 import { UsersModule } from '../src/users/users.module';
-import { Conversation } from '../src/conversations/conversation.entity';
-import { Message } from '../src/messages/message.entity';
 
 describe('AuthModule (e2e)', () => {
   let app: INestApplication;
@@ -19,7 +20,7 @@ describe('AuthModule (e2e)', () => {
         TypeOrmModule.forRoot({
           type: 'sqlite',
           database: ':memory:',
-          entities: [User, Conversation, Message],
+          entities: [User, Conversation, Message, Insight],
           synchronize: true,
         }),
         UsersModule,
